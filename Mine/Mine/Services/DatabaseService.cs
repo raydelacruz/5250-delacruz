33265a2cd381524cd2fd1028981b75dfe077fe53
raydelacruz <76924301+raydelacruz@users.noreply.github.com>
2021-02-02 19:52:25 -0800

    Added Delete to DatabaseService.cs

--- Mine/Mine/Services/DatabaseService.cs
+++ Mine/Mine/Services/DatabaseService.cs
@@ -75,9 +75,26 @@ namespace Mine.Services
             return true;
         }
 
-        public Task<bool> DeleteAsync(string id)
+        /// <summary>
+        /// Deletes data in the database (delete page)
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<bool> DeleteAsync(string id)
         {
-            throw new NotImplementedException();
+            var data = await ReadAsync(id);
+            if(data==null)
+            {
+                return false;
+            }
+
+            var result = await Database.DeleteAsync(data);
+            if(result == 0)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         /// <summary>
